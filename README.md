592c10759fa8bd299ce2d79df6c70ee8cd10a0b5
HALF111 <1275113530@qq.com>
2022-09-06 11:20:16 +0800

    finish lab00-lab02 and hw01

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cs61a
+online course website: https://inst.eecs.berkeley.edu/~cs61a/fa20/
---
+++ a.py
@@ -0,0 +1,18 @@
+def f(n, d):
+    """asddsad
+
+    >>> q, r = f(2013, 10)
+    >>> q
+    201
+    >>> r
+    3
+    """
+    return n // d, n % d
+
+def a():
+    print("a")
+    return "aaa"
+
+def b():
+    print("b")
+    return "bbb"
